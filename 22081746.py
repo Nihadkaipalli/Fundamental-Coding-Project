a345678ac5d50b5bc15b22e727a6e20271cf39a0
Nihadkaipalli <nihadkaipalli@gmail.com>
2024-01-09 21:21:26 +0000

    Edit

--- 22081746.py
+++ 22081746.py
@@ -28,8 +28,6 @@ fraction_between = np.sum((data['Salary'] >= lower_bound) & (
     data['Salary'] <= upper_bound)) / len(data)
 
 # Function to get the statistical description of the dataset
-
-
 def getStatisticalDescription(data):
     mean = np.mean(data)
     median = np.median(data)
@@ -40,7 +38,6 @@ def getStatisticalDescription(data):
     range = max(data) - min(data)
     return mean, mode, median, std, kurtosis, skewness, range, max(data), min(data)
 
-
 mean, mode, median, std, kurtosis, skewness, range, maxV, minV, = getStatisticalDescription(
     data['Salary'])
 print(f"mean : {mean}, mode is : {mode}, median : {median}, std : {std}, kurtosis : {kurtosis}, skewness : {skewness},Range : {range} , Max : {maxV} , Min : {minV}")
@@ -48,10 +45,10 @@ print(f"mean : {mean}, mode is : {mode}, median : {median}, std : {std}, kurtosi
 # Plot mean and fraction on the graph
 plt.axvline(mean_salary, color='red', linestyle='--',
             label=f'Mean Salary: {mean_salary:.2f}')
-plt.text(mean_salary, plt.ylim()[1]*0.9,
+plt.text(mean_salary, plt.ylim()[1]*0.8,
          f'Mean Salary: {mean_salary:.2f}', color='red')
 plt.text(upper_bound, plt.ylim()[
-         1]*0.5, f'Fraction(X)-(W~ to 1.25W~): {fraction_between:.2f}', color='green')
+         1]*0.65, f'Fraction(X): {fraction_between:.2f}', color='green')
 
 # Set labels, title, and legend
 plt.xlabel('Salary')
@@ -60,6 +57,6 @@ plt.title('Salary Distribution and Probability Density Function')
 plt.xlim(0, plt.xlim()[1])
 plt.ylim(0, plt.ylim()[1])
 plt.legend()
-
+plt.tight_layout()
 # Show the plot
 plt.show()
